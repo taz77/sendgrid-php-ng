5cc0b95ba235adcb72948e8afaaddefad69cd0fc
Brady Owens <info@fastglass.net>
2016-02-01 22:38:15 +0100

    Code documentation for responses after emails have been sent.

--- lib/SendGrid/Response.php
+++ lib/SendGrid/Response.php
@@ -2,39 +2,64 @@
 
 namespace SendGrid;
 
-class Response
-{
-    public
-        $code,
-        $headers,
-        $raw_body,
-        $body;
+/**
+ * Class Response
+ * @package SendGrid
+ */
+class Response {
+  public
+    $code,
+    $headers,
+    $raw_body,
+    $body;
 
-    public function __construct($code, $headers, $raw_body, $body)
-    {
-        $this->code = $code;
-        $this->headers = $headers;
-        $this->raw_body = $raw_body;
-        $this->body = $body;
-    }
+  /**
+   * Response constructor.
+   * @param $code
+   * @param $headers
+   * @param $raw_body
+   * @param $body
+   */
+  public function __construct($code, $headers, $raw_body, $body) {
+    $this->code = $code;
+    $this->headers = $headers;
+    $this->raw_body = $raw_body;
+    $this->body = $body;
+  }
 
-    public function getCode()
-    {
-        return $this->code;
-    }
+  /**
+   * Get Response code.
+   *
+   * @return string $this->code
+   */
+  public function getCode() {
+    return $this->code;
+  }
 
-    public function getHeaders()
-    {
-        return $this->headers;
-    }
+  /**
+   * Return message headers after sending.
+   *
+   * @return array $this->headers
+   */
+  public function getHeaders() {
+    return $this->headers;
+  }
 
-    public function getRawBody()
-    {
-        return $this->raw_body;
-    }
+  /**
+   * Return the raw body of the email after sending.
+   *
+   * @return string $this->raw_body
+   */
+  public function getRawBody() {
+    return $this->raw_body;
+  }
 
-    public function getBody()
-    {
-        return $this->body;
-    }
+  /**
+   * Return the body of the email after sending.
+   *
+   * @return string $this->body
+   */
+  public function getBody() {
+    return $this->body;
+  }
 }
