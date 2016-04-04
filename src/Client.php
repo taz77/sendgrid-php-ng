7e6f902b3980b6b5c2132cc24137f3f785990e33
Brady Owens <info@fastglass.net>
2016-04-04 21:49:16 +0200

    Moved autoloading functions out into their own file. For cleanup. Trying to eliminate using Composer.

--- src/Client.php
+++ src/Client.php
@@ -170,22 +170,6 @@ class Client {
     return $response;
   }
 
-  public static function register_autoloader() {
-    spl_autoload_register(['SendGrid/Client', 'autoloader']);
-    //spl_autoload_register(__NAMESPACE__ . '\Client::autoloader');
-  }
-
-  public function autoloader($class) {
-    // Check that the class starts with 'SendGrid'
-    if ($class == 'SendGrid' || stripos($class, 'SendGrid\\') === 0) {
-      $file = str_replace('\\', '/', $class);
-
-      if (file_exists(dirname(__FILE__) . '/' . $file . '.php')) {
-        require_once(dirname(__FILE__) . '/' . $file . '.php');
-      }
-    }
-  }
-
   /**
    * Prepare a request to be submitted as multipart.
    *
