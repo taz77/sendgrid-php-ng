e7de7b0d7af0e388becd8a615fe8c0f26dfd8f42
Brady Owens <info@fastglass.net>
2016-06-30 10:02:36 +0200

    Add documentation to tests and mark the tests that are currently broken.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -22,6 +22,9 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $this->assertEquals(json_decode(file_get_contents('composer.json'))->version, \SendGrid\Client::VERSION);
   }
 
+  /**
+   * Test initializing client with API key.
+   */
   public function testInitWithApiKey() {
     $sendgrid = new s\Client('token123456789');
     $this->assertEquals('SendGrid\Client', get_class($sendgrid));
@@ -29,11 +32,17 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $this->assertEquals($sendgrid->apiKey, 'token123456789');
   }
 
+  /**
+   * Test initializing client with API key and options.
+   */
   public function testInitWithApiKeyOptions() {
     $sendgrid = new s\Client('token123456789', ['foo' => 'bar']);
     $this->assertEquals('SendGrid\Client', get_class($sendgrid));
   }
 
+  /**
+   * Test initializing client with API key with a proxy specified.
+   */
   public function testInitWithProxyOption() {
     $sendgrid = new s\Client('token123456789', ['proxy' => 'myproxy.net:3128']);
     $this->assertEquals('SendGrid\Client', get_class($sendgrid));
@@ -41,17 +50,26 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $this->assertTrue(isset($options['proxy']));
   }
 
+  /**
+   * Test the default URL being returned by the client.
+   */
   public function testDefaultURL() {
     $sendgrid = new s\Client('token123456789');
     $this->assertEquals('https://api.sendgrid.com', $sendgrid->url);
   }
 
+  /**
+   * Test the default endpoint being returned by the client.
+   */
   public function testDefaultEndpoint() {
     $sendgrid = new s\Client('token123456789');
     $this->assertEquals('/api/mail.send.json', $sendgrid->endpoint);
 
   }
 
+  /**
+   * Test creating a client with a custom URL.
+   */
   public function testCustomURL() {
     $options = [
       'protocol' => 'http',
@@ -63,6 +81,9 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $this->assertEquals('http://sendgrid.org:80', $sendgrid->url);
   }
 
+  /**
+   * Test creating a client with SSL verification turned off.
+   */
   public function testSwitchOffSSLVerification() {
     $sendgrid = new s\Client('token123456789', ['turn_off_ssl_verification' => TRUE]);
     $options = $sendgrid->getOptions();
@@ -70,6 +91,10 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
   }
 
   /**
+   * Test to make sure an exception is thrown when a non-200 response is returned.
+   * This test is currently not working.
+   *
+   * @todo fix this test
    * @expectedException \SendGrid\Exception
    */
   public function testSendGridExceptionThrownWhenNot200() {
@@ -92,6 +117,12 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $response = $sendgrid->send($email);
   }
 
+  /**
+   * Test creating a client and disabling exceptions being thrown.
+   * This test is currently not working.
+   *
+   * @todo fix this test
+   */
   public function testDisableSendGridException() {
     $mockResponse = (object) [
       'code' => 400,
@@ -115,6 +146,12 @@ class SendGridTest_Client extends \PHPUnit_Framework_TestCase {
     $response = $sendgrid->send($email);
   }
 
+  /**
+   * Make sure that exceptions do not get thrown if a 200 response is received.
+   * This test is currently not working.
+   *
+   * @todo fix this test
+   */
   public function testSendGridExceptionNotThrownWhen200() {
     $mockResponse = (object) [
       'code' => 200,
