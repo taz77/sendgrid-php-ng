6f6a46225bf6a3daec02a34addf8dbd94e9a745c
Brady Owens <info@fastglass.net>
2016-12-15 23:39:42 -0500

    Class to handle constructing email addresses arrays. New Commit

---
+++ src/EmailAddress.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bowens
+ * Date: 15/12/16
+ * Time: 23:05
+ */
+
+namespace SendGrid;
+
+
+class EmailAddress {
+
+  private
+    $name,
+    $email;
+
+  public function __construct($name, $email) {
+    $this->name = $name;
+    $this->email = $email;
+  }
+
+  public function setName($name) {
+    $this->name = $name;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function setEmail($email) {
+    $this->email = $email;
+  }
+
+  public function getEmail() {
+    return $this->email;
+  }
+
+  public function jsonSerialize() {
+    return array_filter(
+      [
+        'name' => $this->getName(),
+        'email' => $this->getEmail(),
+      ]
+    );
+  }
+}
