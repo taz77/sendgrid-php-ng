08b406310a7dc37eb125e46bae77ef76af30bd37
Brady Owens <info@fastglass.net>
2020-09-21 21:33:15 -0400

    #6 Example file. Wrap stuff in try/catch.

--- examples/mail/mail.php
+++ examples/mail/mail.php
@@ -2,10 +2,10 @@
 
 // Next line will load dependencies to run this example
 // Please refer to the README how to use in your project
-use SendGrid\Mail\Attachment;
-use SendGrid\Mail\GridException;
-use SendGrid\Mail\Mail;
 use SendGrid\Client;
+use SendGrid\Exception\SendgridException;
+use SendGrid\Mail\Attachment;
+use SendGrid\Mail\Mail;
 
 require_once __DIR__ . '/../../sendgrid-php.php';
 
@@ -13,56 +13,60 @@ $email = new Mail();
 try {
   $email->setFrom("test@sg.e.swingsurgeon.link", "Example User");
 }
-catch (GridException $e) {
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
 }
 try {
   $email->setSubject("Sending with Twilio SendGrid is Fun");
 }
-catch (GridException $e) {
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
 }
 try {
   $email->addTo("brady@fastglass.net", "Example User");
 }
-catch (GridException $e) {
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
 }
 try {
   $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 }
-catch (GridException $e) {
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
 }
 try {
   $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
   );
 }
-catch (GridException $e) {
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
+}
+try {
+  $attachment = new Attachment();
+  $attachment->setContent("TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gQ3JhcyBwdW12");
+  $attachment->setType("application/pdf");
+  $attachment->setFilename("balance_001.pdf");
+  $attachment->setDisposition("attachment");
+  $attachment->setContentId("Balance Sheet");
+  $email->addAttachment($attachment);
+}
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
 }
 
-$attachment = new Attachment();
-$attachment->setContent("TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gQ3JhcyBwdW12");
-$attachment->setType("application/pdf");
-$attachment->setFilename("balance_001.pdf");
-$attachment->setDisposition("attachment");
-$attachment->setContentId("Balance Sheet");
-$email->addAttachment($attachment);
-
-$attachment2 = new Attachment();
-$attachment2->setContent("BwdW");
-$attachment2->setType("image/png");
-$attachment2->setFilename("banner.png");
-$attachment2->setDisposition("inline");
-$attachment2->setContentId("Banner");
-$email->addAttachment($attachment2);
-
-// if(property_exists($email, 'attachments')) {
-//   echo "SUCCESS";
-// }
-
-// print_r($email);
-// print_r(json_encode($email, JSON_PRETTY_PRINT));
-
-
-
+try {
+  $attachment2 = new Attachment();
+  $attachment2->setContent("BwdW");
+  $attachment2->setType("image/png");
+  $attachment2->setFilename("banner.png");
+  $attachment2->setDisposition("inline");
+  $attachment2->setContentId("Banner");
+  $email->addAttachment($attachment2);
+}
+catch (SendgridException $e) {
+  // Do something with Exception like log it.
+}
 
 $sendgrid = new Client(getenv('SENDGRID_API_KEY'));
 try {
@@ -71,10 +75,8 @@ try {
   // print_r($response->getHeaders());
   print_r(json_encode($response->getBody(), JSON_PRETTY_PRINT));
 }
-catch (Exception $e) {
-  echo 'Caught exception: ' . $e->getCode() . "\n";
-  echo 'Caught exception: ' . $e->getMessage() . "\n";
-  print_r(json_encode($e->getBody(), JSON_PRETTY_PRINT));
+catch (SendgridException $e) {
+  print_r($e->getCode());
 }
 
 
