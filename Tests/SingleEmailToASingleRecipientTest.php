ce3508590fa70730c099a53a6bc55c389d774402
Brady Owens <info@fastglass.net>
2020-11-17 16:57:55 -0500

    #6  Single Email To A Single Recipient Test

--- Tests/SingleEmailToASingleRecipientTest.php
+++ Tests/SingleEmailToASingleRecipientTest.php
@@ -1,11 +1,10 @@
 <?php
+declare(strict_types=1);
 /**
  * This file tests the request object generation for a /mail/send API call.
  */
 
-namespace SendGrid\Tests\Unit;
-
-use SendGrid\Tests\BaseTestClass;
+namespace SendGrid\Tests;
 
 
 /**
@@ -13,10 +12,9 @@ use SendGrid\Tests\BaseTestClass;
  *
  * @package SendGrid\Tests\Unit
  */
-class SingleEmailToASingleRecipientTest extends BaseTestClass
-{
+class SingleEmailToASingleRecipientTest extends BaseTestClass {
 
-    private $REQUEST_OBJECT = <<<'JSON'
+  private $REQUEST_OBJECT = <<<'JSON'
 {
     "personalizations": [
         {
@@ -46,47 +44,45 @@ class SingleEmailToASingleRecipientTest extends BaseTestClass
 }
 JSON;
 
-    /**
-     * Test all parameters using objects
-     */
-    public function testWithObjects()
-    {
-        $from = new \SendGrid\Mail\From("test@example.com", "Example User");
-        $subject = new \SendGrid\Mail\Subject("Sending with Twilio SendGrid is Fun");
-        $to = new \SendGrid\Mail\To("test@example.com", "Example User");
-        $plainTextContent = new \SendGrid\Mail\PlainTextContent(
-            "and easy to do anywhere, even with PHP"
-        );
-        $htmlContent = new \SendGrid\Mail\HtmlContent(
-            "<strong>and easy to do anywhere, even with PHP</strong>"
-        );
-        $email = new \SendGrid\Mail\Mail(
-            $from,
-            $to,
-            $subject,
-            $plainTextContent,
-            $htmlContent
-        );
-        $json = json_encode($email->jsonSerialize());
-        $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
-        $this->assertTrue($isEqual);
-    }
+  /**
+   * Test all parameters using objects
+   */
+  public function testWithObjects(): void {
+    $from = new \SendGrid\Mail\From("test@example.com", "Example User");
+    $subject = new \SendGrid\Mail\Subject("Sending with Twilio SendGrid is Fun");
+    $to = new \SendGrid\Mail\To("test@example.com", "Example User");
+    $plainTextContent = new \SendGrid\Mail\PlainTextContent(
+      "and easy to do anywhere, even with PHP"
+    );
+    $htmlContent = new \SendGrid\Mail\HtmlContent(
+      "<strong>and easy to do anywhere, even with PHP</strong>"
+    );
+    $email = new \SendGrid\Mail\Mail(
+      $from,
+      $to,
+      $subject,
+      $plainTextContent,
+      $htmlContent
+    );
+    $json = json_encode($email->jsonSerialize());
+    $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
+    $this->assertTrue($isEqual);
+  }
 
-    /**
-     * Test all parameters without using objects
-     */
-    public function testWithoutObjects()
-    {
-        $email = new \SendGrid\Mail\Mail();
-        $email->setFrom("test@example.com", "Example User");
-        $email->setSubject("Sending with Twilio SendGrid is Fun");
-        $email->addTo("test@example.com", "Example User");
-        $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-        $email->addContent(
-            "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-        );
-        $json = json_encode($email->jsonSerialize());
-        $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
-        $this->assertTrue($isEqual);
-    }
+  /**
+   * Test all parameters without using objects
+   */
+  public function testWithoutObjects(): void {
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("test@example.com", "Example User");
+    $email->setSubject("Sending with Twilio SendGrid is Fun");
+    $email->addTo("test@example.com", "Example User");
+    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+    $email->addContent(
+      "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    );
+    $json = json_encode($email->jsonSerialize());
+    $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
+    $this->assertTrue($isEqual);
+  }
 }
