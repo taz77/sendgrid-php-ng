a0e6a5c2d1f39762372175c797182aded4b44f44
Brady Owens <info@fastglass.net>
2020-11-17 17:40:48 -0500

    #6  Correct ASM test for proper exception being thrown

--- Tests/AsmTest.php
+++ Tests/AsmTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 namespace SendGrid\Tests;
 
 use PHPUnit\Framework\TestCase;
-use SendGrid\Exception\SendgridException;
+use SendGrid\Exception\TypeException;
 use SendGrid\Mail\Asm;
 use SendGrid\Mail\GroupId;
 use SendGrid\Mail\GroupsToDisplay;
@@ -40,15 +40,15 @@ class AsmTest extends TestCase {
   }
 
   public function testSetGroupToDisplayOnInvalidValue(): void {
-    $this->expectException(SendgridException::class);
-    $this->expectExceptionMessage("\"\$groups_to_display\" must be an instance of SendGrid\Mail\GroupsToDisplay or an array.");
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$groups_to_display" must be an instance of SendGrid\Mail\GroupsToDisplay or an array.');
     $asm = new Asm(123456, [1, 2, 3, 4]);
     $asm->setGroupsToDisplay('invalid_array');
   }
 
   public function testSetGroupIdOnInvalidGroupId(): void {
-    $this->expectException(SendgridException::class);
-    $this->expectExceptionMessage("\"\$group_id\" must be an instance of SendGrid\Mail\GroupId or an integer.");
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$group_id" must be an instance of SendGrid\Mail\GroupId or an integer.');
     $asm = new Asm(123456, [1, 2, 3, 4]);
     $asm->setGroupId('invalid_group_id');
   }
