22fe2e7478a3af031e2f5659b9a97d23a1b33132
Brady Owens <info@fastglass.net>
2020-11-17 17:02:26 -0500

    #6  Single Email to Multiple Receipients Test

--- Tests/SingleEmailToMultipleRecipientsTest.php
+++ Tests/SingleEmailToMultipleRecipientsTest.php
@@ -1,11 +1,10 @@
 <?php
+declare(strict_types=1);
 /**
  * This file tests the request object generation for a /mail/send API call.
  */
 
-namespace SendGrid\Tests\Unit;
-
-use SendGrid\Tests\BaseTestClass;
+namespace SendGrid\Tests;
 
 
 /**
@@ -13,10 +12,9 @@ use SendGrid\Tests\BaseTestClass;
  *
  * @package SendGrid\Tests\Unit
  */
-class SingleEmailToMultipleRecipientsTest extends BaseTestClass
-{
+class SingleEmailToMultipleRecipientsTest extends BaseTestClass {
 
-    private $REQUEST_OBJECT = <<<'JSON'
+  private $REQUEST_OBJECT = <<<'JSON'
 {
     "personalizations": [
       {
@@ -54,57 +52,54 @@ class SingleEmailToMultipleRecipientsTest extends BaseTestClass
 }
 JSON;
 
+  /**
+   * Test when we are using objects
+   */
+  public function testWithObjects(): void {
+    $from = new \SendGrid\Mail\From("test@example.com", "Example User");
+    $tos = [
+      new \SendGrid\Mail\To("test+test1@example.com", "Example User1"),
+      new \SendGrid\Mail\To("test+test2@example.com", "Example User2"),
+      new \SendGrid\Mail\To("test+test3@example.com", "Example User3"),
+    ];
+    $subject = new \SendGrid\Mail\Subject("Sending with Twilio SendGrid is Fun");
+    $plainTextContent = new \SendGrid\Mail\PlainTextContent(
+      "and easy to do anywhere, even with PHP"
+    );
+    $htmlContent = new \SendGrid\Mail\HtmlContent(
+      "<strong>and easy to do anywhere, even with PHP</strong>"
+    );
+    $email = new \SendGrid\Mail\Mail(
+      $from,
+      $tos,
+      $subject,
+      $plainTextContent,
+      $htmlContent
+    );
+    $json = json_encode($email->jsonSerialize());
+    $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
+    $this->assertTrue($isEqual);
+  }
 
-    /**
-     * Test when we are using objects
-     */
-    public function testWithObjects()
-    {
-        $from = new \SendGrid\Mail\From("test@example.com", "Example User");
-        $tos = [
-            new \SendGrid\Mail\To("test+test1@example.com", "Example User1"),
-            new \SendGrid\Mail\To("test+test2@example.com", "Example User2"),
-            new \SendGrid\Mail\To("test+test3@example.com", "Example User3")
-        ];
-        $subject = new \SendGrid\Mail\Subject("Sending with Twilio SendGrid is Fun");
-        $plainTextContent = new \SendGrid\Mail\PlainTextContent(
-            "and easy to do anywhere, even with PHP"
-        );
-        $htmlContent = new \SendGrid\Mail\HtmlContent(
-            "<strong>and easy to do anywhere, even with PHP</strong>"
-        );
-        $email = new \SendGrid\Mail\Mail(
-            $from,
-            $tos,
-            $subject,
-            $plainTextContent,
-            $htmlContent
-        );
-        $json = json_encode($email->jsonSerialize());
-        $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
-        $this->assertTrue($isEqual);
-    }
-
-    /**
-     * Test when we are not using objects
-     */
-    public function testWithoutObjects()
-    {
-        $email = new \SendGrid\Mail\Mail();
-        $email->setFrom("test@example.com", "Example User");
-        $tos = [
-            "test+test1@example.com" => "Example User1",
-            "test+test2@example.com" => "Example User2",
-            "test+test3@example.com" => "Example User3"
-        ];
-        $email->addTos($tos);
-        $email->setSubject("Sending with Twilio SendGrid is Fun");
-        $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-        $email->addContent(
-            "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-        );
-        $json = json_encode($email->jsonSerialize());
-        $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
-        $this->assertTrue($isEqual);
-    }
+  /**
+   * Test when we are not using objects
+   */
+  public function testWithoutObjects(): void {
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("test@example.com", "Example User");
+    $tos = [
+      "test+test1@example.com" => "Example User1",
+      "test+test2@example.com" => "Example User2",
+      "test+test3@example.com" => "Example User3",
+    ];
+    $email->addTos($tos);
+    $email->setSubject("Sending with Twilio SendGrid is Fun");
+    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+    $email->addContent(
+      "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    );
+    $json = json_encode($email->jsonSerialize());
+    $isEqual = BaseTestClass::compareJSONObjects($json, $this->REQUEST_OBJECT);
+    $this->assertTrue($isEqual);
+  }
 }
