bebc1f274bb5e0025963e8b50e9cf62c5ada4fa5
Brady Owens <info@fastglass.net>
2020-11-17 14:01:10 -0500

    #6  Mail Get Contents Test

--- Tests/MailGetContentsTest.php
+++ Tests/MailGetContentsTest.php
@@ -1,6 +1,7 @@
 <?php
+declare(strict_types=1);
 
-namespace SendGrid\Tests\Unit;
+namespace SendGrid\Tests;
 
 use PHPUnit\Framework\TestCase;
 use SendGrid\Mail\Mail;
@@ -10,25 +11,22 @@ use SendGrid\Mail\Mail;
  *
  * @package SendGrid\Tests\Unit
  */
-class MailGetContentsTest extends TestCase
-{
-    /**
-     * This method tests that array from Mail getContents() returns with
-     * text/plain Content object first when Mail instantiated with text/html
-     * content before text/plain
-     *
-     * @throws \SendGrid\Exception\TypeException
-     */
-    public function testWillReturnPlainContentFirst()
-    {
-        $email = new Mail();
-        $email->setFrom("test@example.com", null);
-        $email->setSubject("Hello World from the Twilio SendGrid PHP Library");
-        $email->addTo("test@example.com", "Test Person");
+class MailGetContentsTest extends TestCase {
 
-        $email->addContent("text/html", "<p>some text here</p>");
-        $email->addContent("text/plain", "some text here");
-
-        $this->assertEquals('text/plain', $email->getContents()[0]->getType());
-    }
+  /**
+   * This method tests that array from Mail getContents() returns with
+   * text/plain Content object first when Mail instantiated with text/html
+   * content before text/plain
+   *
+   * @throws \SendGrid\Exception\TypeException
+   */
+  public function testWillReturnPlainContentFirst(): void {
+    $email = new Mail();
+    $email->setFrom("test@example.com", NULL);
+    $email->setSubject("Hello World from the Twilio SendGrid PHP Library");
+    $email->addTo("test@example.com", "Test Person");
+    $email->addContent("text/html", "<p>some text here</p>");
+    $email->addContent("text/plain", "some text here");
+    $this->assertEquals('text/plain', $email->getContents()[0]->getType());
+  }
 }
