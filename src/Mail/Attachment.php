aac644d360c7004345648c8e98a71db0e5bdaf07
Brady Owens <info@fastglass.net>
2020-11-17 09:13:52 -0500

    #6 Reformat code

--- src/Mail/Attachment.php
+++ src/Mail/Attachment.php
@@ -6,222 +6,220 @@
 namespace SendGrid\Mail;
 
 use JsonSerializable;
-use SendGrid\Helper\Assert;
 use SendGrid\Exception\SendgridException;
+use SendGrid\Helper\Assert;
 
 /**
- * This class is used to construct a Attachment object for the /mail/send API call
+ * This class is used to construct a Attachment object for the /mail/send API
+ * call
  *
  * @package SendGrid\Mail
  */
-class Attachment implements JsonSerializable
-{
-    /** @var $content string Base64 encoded content */
-    private $content;
-    /** @var $type string Mime type of the attachment */
-    private $type;
-    /** @var $filename string File name of the attachment */
-    private $filename;
-    /** @var $disposition string How the attachment should be displayed: inline or attachment, default is attachment */
-    private $disposition;
-    /** @var $content_id string Used when disposition is inline to display the file within the body of the email */
-    private $content_id;
+class Attachment implements JsonSerializable {
 
-	/**
-	 * Optional constructor
-	 *
-	 * @param string $content     Base64 encoded content
-	 * @param string $type        Mime type of the attachment
-	 * @param string $filename    File name of the attachment
-	 * @param string $disposition How the attachment should be displayed: inline
-	 *                            or attachment, default is attachment
-	 * @param string $content_id  Used when disposition is inline to display the
-	 *                            file within the body of the email
-	 * @throws SendgridException
-	 */
-    public function __construct(
-        $content = null,
-        $type = null,
-        $filename = null,
-        $disposition = null,
-        $content_id = null
-    ) {
-        if (isset($content)) {
-            $this->setContent($content);
-        }
-        if (isset($type)) {
-            $this->setType($type);
-        }
-        if (isset($filename)) {
-            $this->setFilename($filename);
-        }
-        if (isset($disposition)) {
-            $this->setDisposition($disposition);
-        }
-        if (isset($content_id)) {
-            $this->setContentID($content_id);
-        }
+  /** @var $content string Base64 encoded content */
+  private $content;
+
+  /** @var $type string Mime type of the attachment */
+  private $type;
+
+  /** @var $filename string File name of the attachment */
+  private $filename;
+
+  /** @var $disposition string How the attachment should be displayed: inline or attachment, default is attachment */
+  private $disposition;
+
+  /** @var $content_id string Used when disposition is inline to display the file within the body of the email */
+  private $content_id;
+
+  /**
+   * Optional constructor
+   *
+   * @param string $content Base64 encoded content
+   * @param string $type Mime type of the attachment
+   * @param string $filename File name of the attachment
+   * @param string $disposition How the attachment should be displayed: inline
+   *                            or attachment, default is attachment
+   * @param string $content_id Used when disposition is inline to display the
+   *                            file within the body of the email
+   *
+   * @throws SendgridException
+   */
+  public function __construct(
+    $content = NULL,
+    $type = NULL,
+    $filename = NULL,
+    $disposition = NULL,
+    $content_id = NULL
+  ) {
+    if (isset($content)) {
+      $this->setContent($content);
     }
-
-    /**
-     * Add the content to a Attachment object
-     *
-     * @param string $content Base64 encoded content
-     *
-     * @throws SendgridException
-     */
-    public function setContent($content)
-    {
-        Assert::minLength($content, 'content', 1);
-
-        if (!$this->isBase64($content)) {
-            $this->content = base64_encode($content);
-        } else {
-            $this->content = $content;
-        }
+    if (isset($type)) {
+      $this->setType($type);
     }
-
-    /**
-     * Retrieve the content from a Attachment object
-     *
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->content;
+    if (isset($filename)) {
+      $this->setFilename($filename);
     }
-
-    /**
-     * Add the mime type to a Attachment object
-     *
-     * @param string $type Mime type of the attachment
-     *
-     * @throws SendgridException
-     */
-    public function setType($type)
-    {
-        Assert::minLength($type, 'type', 1);
-
-        $this->type = $type;
+    if (isset($disposition)) {
+      $this->setDisposition($disposition);
     }
-
-    /**
-     * Retrieve the mime type from a Attachment object
-     *
-     * @return string
-     */
-    public function getType()
-    {
-        return $this->type;
+    if (isset($content_id)) {
+      $this->setContentID($content_id);
     }
+  }
 
-    /**
-     * Add the file name to a Attachment object
-     *
-     * @param string $filename File name of the attachment
-     *
-     * @throws SendgridException
-     */
-    public function setFilename($filename)
-    {
-        Assert::string($filename, 'filename');
+  /**
+   * Add the content to a Attachment object
+   *
+   * @param string $content Base64 encoded content
+   *
+   * @throws SendgridException
+   */
+  public function setContent($content) {
+    Assert::minLength($content, 'content', 1);
 
-        $this->filename = $filename;
+    if (!$this->isBase64($content)) {
+      $this->content = base64_encode($content);
     }
-
-    /**
-     * Retrieve the file name from a Attachment object
-     *
-     * @return string
-     */
-    public function getFilename()
-    {
-        return $this->filename;
+    else {
+      $this->content = $content;
     }
+  }
 
-    /**
-     * Add the disposition to a Attachment object
-     *
-     * @param string $disposition How the attachment should be displayed:
-     *                            inline or attachment, default is attachment
-     *
-     * @throws SendgridException
-     */
-    public function setDisposition($disposition)
-    {
-        Assert::anyOf($disposition, 'disposition', ['inline', 'attachment']);
+  /**
+   * Retrieve the content from a Attachment object
+   *
+   * @return string
+   */
+  public function getContent() {
+    return $this->content;
+  }
 
-        $this->disposition = $disposition;
+  /**
+   * Add the mime type to a Attachment object
+   *
+   * @param string $type Mime type of the attachment
+   *
+   * @throws SendgridException
+   */
+  public function setType($type) {
+    Assert::minLength($type, 'type', 1);
+
+    $this->type = $type;
+  }
+
+  /**
+   * Retrieve the mime type from a Attachment object
+   *
+   * @return string
+   */
+  public function getType() {
+    return $this->type;
+  }
+
+  /**
+   * Add the file name to a Attachment object
+   *
+   * @param string $filename File name of the attachment
+   *
+   * @throws SendgridException
+   */
+  public function setFilename($filename) {
+    Assert::string($filename, 'filename');
+
+    $this->filename = $filename;
+  }
+
+  /**
+   * Retrieve the file name from a Attachment object
+   *
+   * @return string
+   */
+  public function getFilename() {
+    return $this->filename;
+  }
+
+  /**
+   * Add the disposition to a Attachment object
+   *
+   * @param string $disposition How the attachment should be displayed:
+   *                            inline or attachment, default is attachment
+   *
+   * @throws SendgridException
+   */
+  public function setDisposition($disposition) {
+    Assert::anyOf($disposition, 'disposition', ['inline', 'attachment']);
+
+    $this->disposition = $disposition;
+  }
+
+  /**
+   * Retrieve the disposition from a Attachment object
+   *
+   * @return string
+   */
+  public function getDisposition() {
+    return $this->disposition;
+  }
+
+  /**
+   * Add the content id to a Attachment object
+   *
+   * @param string $content_id Used when disposition is inline to display
+   *                           the file within the body of the email
+   *
+   * @throws SendgridException
+   */
+  public function setContentID($content_id) {
+    Assert::string($content_id, 'content_id');
+
+    $this->content_id = $content_id;
+  }
+
+  /**
+   * Retrieve the content id from a Attachment object
+   *
+   * @return string
+   */
+  public function getContentID() {
+    return $this->content_id;
+  }
+
+  /**
+   *  Verifies whether or not the provided string is a valid base64 string
+   *
+   * @param $string string The string that has to be checked
+   *
+   * @return bool
+   */
+  private function isBase64($string) {
+    $decoded_data = base64_decode($string, TRUE);
+    $encoded_data = base64_encode($decoded_data);
+    if ($encoded_data != $string) {
+      return FALSE;
     }
+    return TRUE;
+  }
 
-    /**
-     * Retrieve the disposition from a Attachment object
-     *
-     * @return string
-     */
-    public function getDisposition()
-    {
-        return $this->disposition;
-    }
-
-	/**
-	 * Add the content id to a Attachment object
-	 *
-	 * @param string $content_id Used when disposition is inline to display
-	 *                           the file within the body of the email
-	 * @throws SendgridException
-	 */
-    public function setContentID($content_id)
-    {
-        Assert::string($content_id, 'content_id');
-
-        $this->content_id = $content_id;
-    }
-
-    /**
-     * Retrieve the content id from a Attachment object
-     *
-     * @return string
-     */
-    public function getContentID()
-    {
-        return $this->content_id;
-    }
-
-    /**
-     *  Verifies whether or not the provided string is a valid base64 string
-     *
-     * @param $string string The string that has to be checked
-     * @return bool
-     */
-    private function isBase64($string)
-    {
-        $decoded_data = base64_decode($string, true);
-        $encoded_data = base64_encode($decoded_data);
-        if ($encoded_data != $string) {
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Return an array representing a Attachment object for the Twilio SendGrid API
-     *
-     * @return null|array
-     */
-    public function jsonSerialize()
-    {
-        return array_filter(
-            [
-                'content' => $this->getContent(),
-                'type' => $this->getType(),
-                'filename' => $this->getFilename(),
-                'disposition' => $this->getDisposition(),
-                'content_id' => $this->getContentID()
-            ],
-            function ($value) {
-                return $value !== null;
-            }
-        ) ?: null;
-    }
+  /**
+   * Return an array representing a Attachment object for the Twilio SendGrid
+   * API
+   *
+   * @return null|array
+   */
+  public function jsonSerialize() {
+    return array_filter(
+      [
+        'content' => $this->getContent(),
+        'type' => $this->getType(),
+        'filename' => $this->getFilename(),
+        'disposition' => $this->getDisposition(),
+        'content_id' => $this->getContentID(),
+      ],
+      function ($value) {
+        return $value !== NULL;
+      }
+    ) ?: null;
+  }
 }
