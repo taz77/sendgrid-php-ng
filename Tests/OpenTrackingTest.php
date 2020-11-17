27542afd8441f6af611cabf4005e352d8ffd9d85
Brady Owens <info@fastglass.net>
2020-11-17 14:07:05 -0500

    #6  Open Tracking Test

--- Tests/OpenTrackingTest.php
+++ Tests/OpenTrackingTest.php
@@ -1,61 +1,51 @@
 <?php
+declare(strict_types=1);
 /**
  * This file tests OpenTracking.
  */
 
 namespace SendGrid\Tests;
 
-use SendGrid\Mail\OpenTracking;
 use PHPUnit\Framework\TestCase;
+use SendGrid\Exception\TypeException;
+use SendGrid\Mail\OpenTracking;
 
 /**
  * This class tests OpenTracking.
  *
  * @package SendGrid\Tests
  */
-class OpenTrackingTest extends TestCase
-{
-    public function testConstructor()
-    {
-        $openTracking = new OpenTracking(true, 'substitution_tag');
+class OpenTrackingTest extends TestCase {
 
-        $this->assertTrue($openTracking->getEnable());
-        $this->assertSame('substitution_tag', $openTracking->getSubstitutionTag());
-    }
+  public function testConstructor() {
+    $openTracking = new OpenTracking(TRUE, 'substitution_tag');
+    $this->assertTrue($openTracking->getEnable());
+    $this->assertSame('substitution_tag', $openTracking->getSubstitutionTag());
+  }
 
-    public function testSetEnable()
-    {
-        $openTracking = new OpenTracking();
-        $openTracking->setEnable(true);
+  public function testSetEnable() {
+    $openTracking = new OpenTracking();
+    $openTracking->setEnable(TRUE);
+    $this->assertTrue($openTracking->getEnable());
+  }
 
-        $this->assertTrue($openTracking->getEnable());
-    }
+  public function testSetEnableOnInvalidType() {
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$enable" must be a boolean.');
+    $openTracking = new OpenTracking();
+    $openTracking->setEnable('invalid_bool');
+  }
 
-    /**
-     * @expectedException \SendGrid\Exception\TypeException
-     * @expectedExceptionMessage "$enable" must be a boolean.
-     */
-    public function testSetEnableOnInvalidType()
-    {
-        $openTracking = new OpenTracking();
-        $openTracking->setEnable('invalid_bool');
-    }
+  public function testSetSubstitutionTag() {
+    $openTracking = new OpenTracking();
+    $openTracking->setSubstitutionTag('substitution_tag');
+    $this->assertSame('substitution_tag', $openTracking->getSubstitutionTag());
+  }
 
-    public function testSetSubstitutionTag()
-    {
-        $openTracking = new OpenTracking();
-        $openTracking->setSubstitutionTag('substitution_tag');
-
-        $this->assertSame('substitution_tag', $openTracking->getSubstitutionTag());
-    }
-
-    /**
-     * @expectedException \SendGrid\Exception\TypeException
-     * @expectedExceptionMessage "$substitution_tag" must be a string.
-     */
-    public function testSetSubstitutionTagOnInvalidType()
-    {
-        $openTracking = new OpenTracking();
-        $openTracking->setSubstitutionTag(['invalid_substitution_tag']);
-    }
+  public function testSetSubstitutionTagOnInvalidType() {
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$substitution_tag" must be a string.');
+    $openTracking = new OpenTracking();
+    $openTracking->setSubstitutionTag(['invalid_substitution_tag']);
+  }
 }
