6061f3ca88eebbcf0b4e10363088f6633c70ab3f
Brady Owens <info@fastglass.net>
2020-11-17 17:26:03 -0500

    #6  Substitution Test

--- Tests/SubstitutionTest.php
+++ Tests/SubstitutionTest.php
@@ -1,7 +1,10 @@
 <?php
-namespace SendGrid\Tests\Unit;
+declare(strict_types=1);
+
+namespace SendGrid\Tests;
 
 use PHPUnit\Framework\TestCase;
+use SendGrid\Exception\TypeException;
 use SendGrid\Mail\Substitution;
 
 /**
@@ -9,74 +12,61 @@ use SendGrid\Mail\Substitution;
  *
  * @package SendGrid\Tests\Unit
  */
-class SubstitutionTest extends TestCase
-{
-    public function testSubstitutionSetValue()
-    {
-        $substitution = new Substitution();
-        $testString = 'Twilio SendGrid is awesome!';
-        $testArray = ['one', 'two', 'three'];
-        $testObject = (object) array('1' => 'foo');
-        $testNumberInt = 1;
-        $testNumberFloat = 1.0;
-        $testInvalidInput = null;
+class SubstitutionTest extends TestCase {
 
-        $substitution->setValue($testString);
-        $this->assertEquals($substitution->getValue(), $testString);
-        $substitution->setValue($testArray);
-        $this->assertEquals($substitution->getValue(), $testArray);
-        $substitution->setValue($testObject);
-        $this->assertEquals($substitution->getValue(), $testObject);
-        $substitution->setValue($testNumberInt);
-        $this->assertEquals($substitution->getValue(), $testNumberInt);
-        $substitution->setValue($testNumberFloat);
-        $this->assertEquals($substitution->getValue(), $testNumberFloat);
-        $this->expectException('SendGrid\Exception\TypeException');
-        $substitution->setValue($testInvalidInput);
-    }
+  public function testSubstitutionSetValue(): void {
+    $substitution = new Substitution();
+    $testString = 'Twilio SendGrid is awesome!';
+    $testArray = ['one', 'two', 'three'];
+    $testObject = (object) ['1' => 'foo'];
+    $testNumberInt = 1;
+    $testNumberFloat = 1.0;
+    $testInvalidInput = NULL;
 
-    public function testConstructor()
-    {
-        $substitution = new Substitution('key', 'value');
+    $substitution->setValue($testString);
+    $this->assertEquals($substitution->getValue(), $testString);
+    $substitution->setValue($testArray);
+    $this->assertEquals($substitution->getValue(), $testArray);
+    $substitution->setValue($testObject);
+    $this->assertEquals($substitution->getValue(), $testObject);
+    $substitution->setValue($testNumberInt);
+    $this->assertEquals($substitution->getValue(), $testNumberInt);
+    $substitution->setValue($testNumberFloat);
+    $this->assertEquals($substitution->getValue(), $testNumberFloat);
+    $this->expectException('SendGrid\Exception\TypeException');
+    $substitution->setValue($testInvalidInput);
+  }
 
-        $this->assertInstanceOf(Substitution::class, $substitution);
-        $this->assertSame('key', $substitution->getKey());
-        $this->assertSame('value', $substitution->getValue());
-    }
+  public function testConstructor(): void {
+    $substitution = new Substitution('key', 'value');
+    $this->assertInstanceOf(Substitution::class, $substitution);
+    $this->assertSame('key', $substitution->getKey());
+    $this->assertSame('value', $substitution->getValue());
+  }
 
-    public function testSetKey()
-    {
-        $substitution = new Substitution();
-        $substitution->setKey('key');
+  public function testSetKey(): void {
+    $substitution = new Substitution();
+    $substitution->setKey('key');
+    $this->assertSame('key', $substitution->getKey());
+  }
 
-        $this->assertSame('key', $substitution->getKey());
-    }
+  public function testSetKeyOnInvalidType(): void {
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$key" must be a string');
+    $substitution = new Substitution();
+    $substitution->setKey(TRUE);
+  }
 
-    /**
-     * @expectedException \SendGrid\Exception\TypeException
-     * @expectedExceptionMessage "$key" must be a string.
-     */
-    public function testSetKeyOnInvalidType()
-    {
-        $substitution = new Substitution();
-        $substitution->setKey(true);
-    }
+  public function testSetValue(): void {
+    $substitution = new Substitution();
+    $substitution->setValue('key');
+    $this->assertSame('key', $substitution->getValue());
+  }
 
-    public function testSetValue()
-    {
-        $substitution = new Substitution();
-        $substitution->setValue('key');
-
-        $this->assertSame('key', $substitution->getValue());
-    }
-
-    /**
-     * @expectedException \SendGrid\Exception\TypeException
-     * @expectedExceptionMessage "$value" must be an array, object, boolean, string, numeric or integer.
-     */
-    public function testSetValueOnInvalidType()
-    {
-        $substitution = new Substitution();
-        $substitution->setValue(null);
-    }
+  public function testSetValueOnInvalidType(): void {
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$value" must be an array, object, boolean, string, numeric or integer.');
+    $substitution = new Substitution();
+    $substitution->setValue(NULL);
+  }
 }
