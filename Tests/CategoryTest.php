06892df7eaafd4593c3407b2295f636af5dbe860
Brady Owens <info@fastglass.net>
2020-11-17 10:26:07 -0500

    #6  Category Test

--- Tests/CategoryTest.php
+++ Tests/CategoryTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /**
  * This file tests Category.
  */
@@ -7,6 +8,7 @@ namespace SendGrid\Tests;
 
 use PHPUnit\Framework\TestCase;
 use SendGrid\Mail\Category;
+use SendGrid\Exception\TypeException;
 
 /**
  * This file tests Category.
@@ -15,30 +17,28 @@ use SendGrid\Mail\Category;
  */
 class CategoryTest extends TestCase {
 
-  public function testConstructor() {
+  public function testConstructor(): void {
     $category = new Category('category');
 
     $this->assertInstanceOf(Category::class, $category);
     $this->assertSame('category', $category->getCategory());
   }
 
-  public function testSetCategory() {
+  public function testSetCategory(): void {
     $category = new Category();
     $category->setCategory('category');
 
     $this->assertSame('category', $category->getCategory());
   }
-
-  /**
-   * @expectedException \SendGrid\Exception\TypeException
-   * @expectedExceptionMessage "$category" must be a string.
-   */
-  public function testSetCategoryOnInvalidType() {
+  
+  public function testSetCategoryOnInvalidType(): void {
+    $this->expectException(TypeException::class);
+    $this->expectExceptionMessage('"$category" must be a string.');
     $category = new Category();
     $category->setCategory(['invalid_category']);
   }
 
-  public function testJsonSerialize() {
+  public function testJsonSerialize(): void {
     $category = new Category();
     $category->setCategory('category');
 
