f6693dadf61ff11f2854428ed192d3baf345d40e
Brady Owens <info@fastglass.net>
2020-11-18 14:03:13 -0500

    #6 Update File exists test for removing Travis file

--- Tests/FilesExistTest.php
+++ Tests/FilesExistTest.php
@@ -21,9 +21,10 @@ class FilesExistTest extends TestCase {
   public function testFilesArePresentInRepo(): void {
     $rootDir = __DIR__ . '/..';
     $this->assertFileExists("$rootDir/.gitignore");
-    $this->assertFileExists("$rootDir/.travis.yml");
+    $this->assertFileExists("$rootDir/.github/workflows/php.yml");
     $this->assertFileExists("$rootDir/CHANGELOG.md");
     $this->assertFileExists("$rootDir/MIT.LICENSE");
+    $this->assertFileExists("$rootDir/composer.json");
     $this->assertFileExists("$rootDir/README.md");
   }
 }
